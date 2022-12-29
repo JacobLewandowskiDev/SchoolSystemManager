4e056e4c938f346e10b80924483b2dc1dcf63032
Jacob <lewandowskiJakub9@gmail.com>
2022-12-28 20:36:54 -0500

    Initial commit

---
+++ src/main/java/com/jakub/SchoolSystemManager/SchoolSystemManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jakub.SchoolSystemManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSystemManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSystemManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakub/SchoolSystemManager/SchoolSystemManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakub.SchoolSystemManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSystemManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
