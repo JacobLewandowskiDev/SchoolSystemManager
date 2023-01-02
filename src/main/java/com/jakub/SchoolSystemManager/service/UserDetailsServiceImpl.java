e19b7fc8737cd98980ef3aa893511f78202ebaf6
Jacob <lewandowskiJakub9@gmail.com>
2023-01-01 22:13:58 -0500

    This commit fixes some issues that are caused by the custom userDetailsService

--- src/main/java/com/jakub/SchoolSystemManager/service/UserDetailsServiceImpl.java
+++ src/main/java/com/jakub/SchoolSystemManager/service/UserDetailsServiceImpl.java
@@ -1,42 +1,42 @@
-package com.jakub.SchoolSystemManager.service;
-
-import com.jakub.SchoolSystemManager.model.AppUser;
-import com.jakub.SchoolSystemManager.model.Role;
-import com.jakub.SchoolSystemManager.repository.UserRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-@Service
-public class UserDetailsServiceImpl implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
-
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        AppUser appUser = userRepository.findUserByEmail(username);
-        if(appUser != null) {
-            return new User(appUser.getEmail(), appUser.getPassword(), buildSimpleGrantedAuthorities(appUser.getRoles()));
-        } else {
-            throw new UsernameNotFoundException("User was not found with email:");
-        }
-    }
-
-    private static List<SimpleGrantedAuthority> buildSimpleGrantedAuthorities(final Set<Role> roles) {
-        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-        for (Role role : roles) {
-            authorities.add(new SimpleGrantedAuthority(role.getName()));
-        }
-        return authorities;
-    }
-}
+//package com.jakub.SchoolSystemManager.service;
+//
+//import com.jakub.SchoolSystemManager.model.AppUser;
+//import com.jakub.SchoolSystemManager.model.Role;
+//import com.jakub.SchoolSystemManager.repository.UserRepository;
+//import jakarta.transaction.Transactional;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Set;
+//
+//@Service
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Override
+//    @Transactional
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        AppUser appUser = userRepository.findUserByEmail(username);
+//        if(appUser != null) {
+//            return new User(appUser.getEmail(), appUser.getPassword(), buildSimpleGrantedAuthorities(appUser.getRoles()));
+//        } else {
+//            throw new UsernameNotFoundException("User was not found with email:");
+//        }
+//    }
+//
+//    private static List<SimpleGrantedAuthority> buildSimpleGrantedAuthorities(final Set<Role> roles) {
+//        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+//        for (Role role : roles) {
+//            authorities.add(new SimpleGrantedAuthority(role.getName()));
+//        }
+//        return authorities;
+//    }
+//}
